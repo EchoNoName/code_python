5606758a794a8f7690572e80f9213639d9ab5d65
EchoNoName <gu.ethan88@gmail.com>
2024-10-01 00:20:31 -0400

    made a screen

--- Independent_Programming_Project/main.py
+++ Independent_Programming_Project/main.py
@@ -1,2 +1,17 @@
 import pygame
 
+pygame.init() #initilize Pygame
+
+height = 600
+width = 1000
+
+screen = pygame.display.set_mode((width, height)) #Making the Screen
+
+#game loop
+run = True
+while run: #while game is running
+    for event in pygame.event.get(): #Retrives events
+        if event.type == pygame.QUIT: #if the X at the top right is clicked
+            run = False #turns off game
+
+
