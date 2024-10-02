1bcf0e4ec33eb45b4e54b3b4a8b030734e4e3966
EchoNoName <gu.ethan88@gmail.com>
2024-10-02 18:13:03 -0400

    test code

--- Independent_Programming_Project/main.py
+++ Independent_Programming_Project/main.py
@@ -9,6 +9,7 @@ def game(): #the game function
         for event in pygame.event.get(): #Retrives events
             if event.type == pygame.QUIT: #if the X at the top right is clicked
                 run = False #turns off game
+                break
 
 height = 600
 width = 1000
