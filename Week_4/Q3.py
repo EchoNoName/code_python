a70831babd2befbfef93c752a9fcea357068dd06
EchoNoName <gu.ethan88@gmail.com>
2024-10-15 09:55:41 -0400

    coded Q11 is week 4 and moved Q12

--- Week_4/Q3.py
+++ Week_4/Q3.py
@@ -7,6 +7,19 @@ def isPalindrome(word):
     returns: 
         A true or false value representing wether a word is a palindrome
     '''
-    return(word == word[::-1])
+    if not word:
+        return True
+    elif len(word) == 1:
+        return True
+    elif len(word) <= 3:
+        if word[0] == word[-1]:
+            return True
+        else:
+            return False
+    else:
+        for i in range(len(word) // 2):
+            if word[i] != word[-i - 1]:
+                return False
+    return True
 
 print(isPalindrome('tacocat'))
