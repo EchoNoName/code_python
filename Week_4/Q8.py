adbf0f354f79fdd7ca2bf5b40d32747403e81718
EchoNoName <gu.ethan88@gmail.com>
2024-10-16 09:33:25 -0400

    Fixed issue with mean not working with even number length lists

--- Week_4/Q8.py
+++ Week_4/Q8.py
@@ -10,13 +10,13 @@ def meanMedFinder(numList: list):
     numList = sorted(numList)   
     med = 0
     if len(numList) % 2 == 0:
-        med = (numList[len(numList)] + numList[len(numList) - 1]) / 2
+        med = (numList[len(numList) // 2] + numList[len(numList) // 2 - 1]) / 2
     else:
         med = (numList[len(numList) // 2])
     mean = sum(numList) / len(numList)
     return (mean, med)
 
-meanMed = meanMedFinder([1, 1, 2, 3, 4, 5, 7, 7, 7])
+meanMed = meanMedFinder([1, 1, 2, 3, 5, 7, 7, 7])
 print(f"The mean is {meanMed[0]}")
 print(f"the median is {meanMed[1]}")
 
