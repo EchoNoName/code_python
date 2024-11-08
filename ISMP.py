537038eab380e2d292b6baedf774a3aa31d3d76a
EchoNoName <gu.ethan88@gmail.com>
2024-11-07 21:35:10 -0500

    ISMP

--- ISMP.py
+++ ISMP.py
@@ -7,30 +7,17 @@ def interval_scheduling(intervals):
     # Track the end time of the last added interval
     last_end_time = float('-inf')
 
-    count = 1
-    #Initiate count
-
-    while intervals:
-        selected_intervals.append(count)
-        #uses count to seperate the seperate intervals schedules
-        for i, interval in enumerate(intervals):
-            start, end = interval
-            #Loops through the intervals and assigns start and end values using tuples
-            # If the start time of the current interval is after the last end time, select it
-            if start >= last_end_time:
-                selected_intervals.append(interval)
-                intervals[i] = 0
-                #Make used interval 0
-                last_end_time = end
-        intervals = [i for i in intervals if i != 0]
-        #removes all used intervals
-        count += 1
-        last_end_time = float('-inf')
-        #reset end time for next loop
+    for interval in intervals:
+        # Unpacks the tuples
+        start, end = interval
+        # If the start time of the current interval is after the last end time, select it
+        if start >= last_end_time:
+            # Add the Interval to the set of Selected Intervals
+            selected_intervals.append(interval)
+            # Update the end time to the end time of the newest interval
+            last_end_time = end
     
     return selected_intervals
 
 intervals = [(1, 3), (2, 5), (4, 7), (6, 9), (8, 10)]
 print(interval_scheduling(intervals))
-
-
